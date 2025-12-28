682520283ecc6e634751f1e7a3e0dd9bb3605814
ruhan-2908 <ruhankb29@gmail.com>
2025-12-28 18:28:58 +0530

    Modify Login and Register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,25 @@
 import { useState } from "react";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+import Auth from "./pages/Auth/Auth";
 import ContentView from "./pages/ContentView/ContentView";
 import Heirarchy from "./pages/Heirarchy/Heirarchy";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<"login" | "register" | "content" | "heirarchy">("login");
+  const [currentPage, setCurrentPage] = useState<"auth" | "content" | "heirarchy">("auth");
 
-  const navigateToRegister = () => setCurrentPage("register");
-  const navigateToLogin = () => setCurrentPage("login");
+  const navigateToAuth = () => setCurrentPage("auth");
   const navigateToContent = () => setCurrentPage("content");
   const navigateToHeirarchy = () => setCurrentPage("heirarchy");
 
   const renderPage = () => {
     switch (currentPage) {
-      case "login":
-        return <Login onNavigateToRegister={navigateToRegister} onNavigateToContent={navigateToContent} />;
-      case "register":
-        return <Register onNavigateToLogin={navigateToLogin} />;
+      case "auth":
+        return <Auth onNavigateToContent={navigateToContent} />;
       case "content":
-        return <ContentView onNavigateToLogin={navigateToLogin} onNavigateToHeirarchy={navigateToHeirarchy} />;
+        return <ContentView onNavigateToLogin={navigateToAuth} onNavigateToHeirarchy={navigateToHeirarchy} />;
       case "heirarchy":
-        return <Heirarchy onNavigateToLogin={navigateToLogin} onNavigateToContent={navigateToContent} />;
+        return <Heirarchy onNavigateToLogin={navigateToAuth} onNavigateToContent={navigateToContent} />;
       default:
-        return <Login onNavigateToRegister={navigateToRegister} onNavigateToContent={navigateToContent} />;
+        return <Auth onNavigateToContent={navigateToContent} />;
     }
   };
 
